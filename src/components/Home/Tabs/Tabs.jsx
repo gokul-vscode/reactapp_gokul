cbc36b92249f05f777149c5e93db0b92024b8a11
gokul-vscode <ramgokul637@gmail.com>
2025-08-28 12:56:22 +0530

    new updated

--- src/components/Home/Tabs/Tabs.jsx
+++ src/components/Home/Tabs/Tabs.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import "../Tabs/Tabs.css";
 import { BsCart2 } from "react-icons/bs";
 import { MdKeyboardDoubleArrowUp } from "react-icons/md";
